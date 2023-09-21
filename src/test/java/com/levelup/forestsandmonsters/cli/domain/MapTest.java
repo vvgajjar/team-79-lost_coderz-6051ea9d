25688b5dbcfe9c5fb41d4a2ff6613c74ae98a5c4
Deep Puri <deeperside@gmail.com>
2023-09-21 15:30:53 +0000

    added map test

--- src/test/java/com/levelup/forestsandmonsters/cli/domain/MapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/cli/domain/MapTest.java
@@ -1,13 +1,29 @@
 package com.levelup.forestsandmonsters.cli.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.Test;
+import java.awt.Point;
 
 public class MapTest {
+
+
+
     @Test
-    public void initializeCharacter() {
+    public void initalizeMap() {
         Map testObj = new Map();
         assertNotNull(testObj);
     }
+
+    @Test
+    public void initalMapSize() {
+        assertEquals(100, Map.numPositions);
+    }
+
+   @Test
+    public void getMapPosition() {
+        ass
+    }    
+
 }

347efb28db05a64f1d6b83dc763d022009695519
Deep Puri <deeperside@gmail.com>
2023-09-21 15:29:54 +0000

    added Map and Map test

--- src/main/java/com/levelup/forestsandmonsters/cli/domain/Map.java
+++ src/main/java/com/levelup/forestsandmonsters/cli/domain/Map.java
@@ -1,8 +1,14 @@
 package com.levelup.forestsandmonsters.cli.domain;
 
+import java.awt.Point;
+
 public class Map {
+    public static int numPositions;
     public Map(){
-        
+        numPositions=100;
+    }
+    public Point getMapPosition(){
+
     }
 
 }

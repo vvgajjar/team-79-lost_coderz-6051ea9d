f630398893960b33c1554799f8adb3f0f64da55d
Deep Puri <deeperside@gmail.com>
2023-09-21 15:26:14 +0000

    added PositionText

---
+++ src/test/java/com/levelup/forestsandmonsters/cli/domain/PositionTest.java
@@ -0,0 +1,18 @@
+package com.levelup.forestsandmonsters.cli.domain;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.Test;
+import java.awt.Point;
+
+public class PositionTest {
+    int mapPostions;
+
+    @Test
+    public void initalizeMap() {
+        Map testObj = new Map();
+        assertNotNull(testObj);
+    }
+}

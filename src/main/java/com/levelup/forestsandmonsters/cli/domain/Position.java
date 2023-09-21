2fa0c2597ba74d101d31b182e49bd8c2481f8e34
JimboSlice <jimmysarath@gmail.com>
2023-09-21 19:41:07 +0000

    change the position class added code

--- src/main/java/com/levelup/forestsandmonsters/cli/domain/Position.java
+++ src/main/java/com/levelup/forestsandmonsters/cli/domain/Position.java
@@ -1,5 +1,28 @@
 package com.levelup.forestsandmonsters.cli.domain;
 
+import java.awt.Point;;
+
 public class Position {
-    
+
+    private Point location;
+
+    public Position(int xCoordinates, int yCoordinates){
+        location = new Point(xCoordinates, yCoordinates);
+    }
+
+    public int getXCoordinates(int xCoordinates){
+        return location.x;
+    }
+     public int getYCoordinates(int yCoordinates){
+        return location.y;
+    }
+     
+    public void setXCoordinates(int xCoordinates){
+
+    }
+
+     public void setYCoordinates(int yCoordinates){
+
+    }
+
 }
